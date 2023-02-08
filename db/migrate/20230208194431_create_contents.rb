f98b7995cfc6c9b2ebbd16731a811c70af18e1ea
Oluwapelumi Odimayo <oluwapelumiodimayo@Oluwapelumis-MacBook-Pro.local>
2023-02-08 13:44:46 -0600

    Content

---
+++ db/migrate/20230208194431_create_contents.rb
@@ -0,0 +1,13 @@
+class CreateContents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contents do |t|
+      t.references :team, null: false, foreign_key: true
+      t.string :title
+      t.text :desxription
+      t.text :url
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/factories/contents.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :content do
+    team { nil }
+    title { "MyString" }
+    desxription { "MyText" }
+    url { "MyText" }
+    active { false }
+  end
+end
